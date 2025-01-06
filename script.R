e43aab748770129ac4a226dbc145df91033479c9
Michael Agyemang <ma24ahs@herts.ac.uk>
2025-01-06 22:01:16 +0000

    Update script.R

--- script.R
+++ script.R
@@ -25,7 +25,7 @@ if (!dir.exists(output_dir)) {
 
 # ---- Statistical Tests ----
 
-# Perform correlation tests 1
+# Perform correlation tests 
 cor_beer <- cor.test(data_clean$HappinessScore, data_clean$Beer_PerCapita)
 cor_spirits <- cor.test(data_clean$HappinessScore, data_clean$Spirit_PerCapita)
 cor_wine <- cor.test(data_clean$HappinessScore, data_clean$Wine_PerCapita)
